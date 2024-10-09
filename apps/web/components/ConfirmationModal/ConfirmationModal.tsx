cab33fc3a799da1b3ef3661ebfae31a91c0c4134
skk <skk@soledad.local>
2024-10-09 08:57:55 +0000

    feat(build): introduce devcontainer

--- apps/web/components/ConfirmationModal/ConfirmationModal.tsx
+++ apps/web/components/ConfirmationModal/ConfirmationModal.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Lottie from 'lottie-react';
+// import Lottie from 'lottie-react';
 import { modals } from '@mantine/modals';
 import { CONSTANTS, colors } from '@config';
 import { Button, Stack, Text } from '@mantine/core';
 
-import FailedAnimationData from './failed-animation-data.json';
-import SuccessAnimationData from './success-animation-data.json';
+/*
+ * import FailedAnimationData from './failed-animation-data.json';
+ * import SuccessAnimationData from './success-animation-data.json';
+ */
 
 interface ConfirmationModalProps {
   status: string;
@@ -14,10 +16,12 @@ interface ConfirmationModalProps {
 export const ConfirmationModal = ({ status }: ConfirmationModalProps) => {
   return (
     <Stack spacing="md">
+      {/*
       <Lottie
         animationData={status === CONSTANTS.PAYMENT_SUCCCESS_CODE ? SuccessAnimationData : FailedAnimationData}
         loop={true}
       />
+      */}
       <Text align="center">
         {status === CONSTANTS.PAYMENT_SUCCCESS_CODE
           ? CONSTANTS.PAYMENT_SUCCESS_MESSAGE
